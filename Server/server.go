759fd73e7bb3aacf1e5ef50d5371d3a7bc09c0e0
DorukGercel <dorukgercel@hotmail.com>
2021-12-25 01:28:10 +0300

    server[feat]: create server

---
+++ Server/server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"GoChatApp/Server/services/cli_parser"
+	"GoChatApp/Server/services/error_handler"
+	"GoChatApp/Server/services/network"
+)
+
+func main() {
+	// Get and check port no given as terminal argument
+	connPortNo, err := cli_parser.Parse()
+	error_handler.CheckFatalError(err)
+	network.AcceptConnection(connPortNo)
+}
